7f2babc6105d9f32aca000b4ca55b969cb657bc0
Emrys Corbin <emrys@corbin.dev>
2024-01-22 16:49:00 -0600

    migrate route transition

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -11,6 +11,7 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideHttpClient(),
     provideMarkdown(),
+    provideAnimations(),
     provideRouter(routes),
     provideClientHydration(),
     provideAnimations()
