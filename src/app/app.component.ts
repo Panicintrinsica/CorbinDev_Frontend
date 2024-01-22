3bf2e9dc940aed27d0c020b7cf36631256c1b130
Emrys Corbin <emrys@corbin.dev>
2024-01-22 16:48:24 -0600

    update for new env files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,24 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import {ChildrenOutletContexts, RouterOutlet} from '@angular/router';
+import {UiNavComponent} from "./components/ui-nav/ui-nav.component";
+import {UiFooterComponent} from "./components/ui-footer/ui-footer.component";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {slideInAnimation} from "./animations/route.anim";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, UiNavComponent, UiFooterComponent],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [ slideInAnimation ]
 })
 export class AppComponent {
-  title = 'corbin';
+  title = 'corbin.dev';
+    constructor(private contexts: ChildrenOutletContexts) {}
+
+  getRouteAnimationData() {
+    return this.contexts.getContext('primary')?.route?.snapshot?.data?.['animation'];
+  }
 }
