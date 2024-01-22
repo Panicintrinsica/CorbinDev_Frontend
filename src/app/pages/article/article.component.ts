c7de495e96333458c777c44a2c8e0164c6a29e88
Emrys Corbin <emrys@corbin.dev>
2024-01-22 17:01:40 -0600

    Migrate article component

--- src/app/pages/article/article.component.ts
+++ src/app/pages/article/article.component.ts
@@ -1,12 +1,38 @@
 import { Component } from '@angular/core';
+import {MatButton} from "@angular/material/button";
+import {ActivatedRoute, RouterLink} from "@angular/router";
+import {UiArticleComponent} from "../../components/ui-article/ui-article.component";
+import {AsyncPipe} from "@angular/common";
+import {Observable} from "rxjs";
+import {Article} from "../../models/article.model";
+import {ServerService} from "../../services/server.service";
 
 @Component({
   selector: 'app-article',
   standalone: true,
-  imports: [],
+  imports: [
+    MatButton,
+    RouterLink,
+    UiArticleComponent,
+    AsyncPipe
+  ],
   templateUrl: './article.component.html',
   styleUrl: './article.component.scss'
 })
 export class ArticleComponent {
+  article$: Observable<Article> | undefined
 
+  constructor(server: ServerService, route: ActivatedRoute) {
+
+    route.paramMap.subscribe(params => {
+      const slug = params.get('slug')
+
+      if(slug){
+        this.article$ = server.getArticle(slug)
+      }
+
+    });
+
+
+  }
 }
