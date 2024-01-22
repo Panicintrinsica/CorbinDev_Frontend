47cd44a85ae03138d75049d01b76b7c59e564a2e
Emrys Corbin <emrys@corbin.dev>
2024-01-22 16:37:57 -0600

    Add markdown and httpClient providers

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,7 +4,15 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import {provideHttpClient} from "@angular/common/http";
+import {provideMarkdown} from "ngx-markdown";
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration(), provideAnimations()]
+  providers: [
+    provideHttpClient(),
+    provideMarkdown(),
+    provideRouter(routes),
+    provideClientHydration(),
+    provideAnimations()
+  ]
 };
