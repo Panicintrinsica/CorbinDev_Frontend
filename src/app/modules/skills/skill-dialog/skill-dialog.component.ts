46c28d7379682358e31243e1128648ab6fd6bee7
Emrys Corbin <emrys.corbin@gmail.com>
2024-08-27 14:43:42 -0500

    Style updates and refactors. Re-link projects to cv.

--- src/app/modules/skills/skill-dialog/skill-dialog.component.ts
+++ src/app/modules/skills/skill-dialog/skill-dialog.component.ts
@@ -1,21 +1,28 @@
-import {Component, Inject} from '@angular/core';
-import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material/dialog";
-import {Observable} from "rxjs";
-import {Skill} from "../../../models/skill.model";
-import {ProjectLink} from "../../../models/project.model";
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { Observable } from 'rxjs';
+import { Skill } from '../../../models/skill.model';
+import { ProjectLink } from '../../../models/project.model';
 
-import {Router} from "@angular/router";
-import {AsyncPipe, DatePipe, NgClass, NgForOf, NgIf, NgOptimizedImage} from "@angular/common";
-import {MarkdownComponent} from "ngx-markdown";
-import {NamedSkillLevel} from "../../../pipes/skill-named-level.pipe";
+import { Router } from '@angular/router';
+import {
+  AsyncPipe,
+  DatePipe,
+  NgClass,
+  NgForOf,
+  NgIf,
+  NgOptimizedImage,
+} from '@angular/common';
+import { MarkdownComponent } from 'ngx-markdown';
+import { NamedSkillLevel } from '../../../pipes/skill-named-level.pipe';
 
-import {SkillService} from "../../../services/skill.service";
-import {ProjectService} from "../../../services/project.service";
+import { SkillService } from '../../../services/skill.service';
+import { ProjectService } from '../../../services/project.service';
 
-import {ProjectListAnim} from "../../../animations/list.anim";
-import {UiSpinnerComponent} from "../../ui/ui-spinner/ui-spinner.component";
-import {TagComponent} from "../../ui/ui-tag/tag.component";
-
+import { ProjectListAnim } from '../../../animations/list.anim';
+import { UiSpinnerComponent } from '../../ui/ui-spinner/ui-spinner.component';
+import { TagComponent } from '../../ui/ui-tag/tag.component';
+import { MatAnchor, MatButton } from '@angular/material/button';
 
 @Component({
   selector: 'ui-skill-dialog',
@@ -32,31 +39,32 @@ import {TagComponent} from "../../ui/ui-tag/tag.component";
     UiSpinnerComponent,
     NgClass,
     TagComponent,
+    MatButton,
+    MatAnchor,
   ],
   templateUrl: './skill-dialog.component.html',
-  styleUrl: './skill-dialog.component.scss'
+  styleUrl: './skill-dialog.component.scss',
 })
 export class SkillDialogComponent {
-  skill$: Observable<Skill> | undefined
-  projects$: Observable<ProjectLink[]> | undefined
+  skill$: Observable<Skill> | undefined;
+  projects$: Observable<ProjectLink[]> | undefined;
 
-  constructor
-  (@Inject(MAT_DIALOG_DATA)
-   public data: string,
-   public projectService: ProjectService,
-   public skillService: SkillService,
-   private router: Router,
-   private dialogRef: MatDialogRef<SkillDialogComponent>
+  constructor(
+    @Inject(MAT_DIALOG_DATA)
+    public data: string,
+    public projectService: ProjectService,
+    public skillService: SkillService,
+    private router: Router,
+    private dialogRef: MatDialogRef<SkillDialogComponent>,
   ) {}
 
   ngOnInit() {
-    this.skill$ = this.skillService.getSkillById(this.data)
-    this.projects$ = this.projectService.getProjectsBySkill(this.data)
+    this.skill$ = this.skillService.getSkillById(this.data);
+    this.projects$ = this.projectService.getProjectsBySkill(this.data);
   }
 
   goToProject(projectSlug: string) {
     this.router.navigateByUrl(`project/${projectSlug}`).then(() => {});
-    this.dialogRef.close()
+    this.dialogRef.close();
   }
-
 }
--- src/app/services/server.service.ts
+++ src/app/services/server.service.ts
@@ -1,89 +1,96 @@
 import { Injectable } from '@angular/core';
-import {Observable} from "rxjs";
-import { HttpClient } from "@angular/common/http";
-import {Article, ArticlePage} from "../models/article.model";
-import {Skill} from "../models/skill.model";
-import {Project, ProjectStub} from "../models/project.model";
-import {Education} from "../models/education.model";
-import {environment} from "../../environments/environment";
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Article, ArticlePage } from '../models/article.model';
+import { Skill } from '../models/skill.model';
+import { Project, ProjectStub } from '../models/project.model';
+import { Education } from '../models/education.model';
+import { environment } from '../../environments/environment';
 import { Detail } from '../models/detail.model';
-import {ContentBlock} from "../models/content.model";
+import { ContentBlock } from '../models/content.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ServerService {
-  private API = environment.API
+  private API = environment.API;
 
   constructor(private http: HttpClient) {}
 
-  getArticle(slug: string): Observable<Article>{
-    return this.http.get<Article>(`${this.API}/articles/${slug}`)
+  getArticle(slug: string): Observable<Article> {
+    return this.http.get<Article>(`${this.API}/articles/${slug}`);
   }
 
-  getArticlePage(): Observable<ArticlePage>{
-    return this.http.get<ArticlePage>(`${this.API}/articles`)
+  getArticlePage(): Observable<ArticlePage> {
+    return this.http.get<ArticlePage>(`${this.API}/articles`);
   }
 
-  getAllDetails(): Observable<Detail[]>{
-    return this.http.get<Detail[]>(`${this.API}/details`)
+  getAllDetails(): Observable<Detail[]> {
+    return this.http.get<Detail[]>(`${this.API}/details`);
   }
 
-  getProfileDetails(): Observable<Detail[]>{
-    return this.http.get<Detail[]>(`${this.API}/details/profile`)
+  getProfileDetails(): Observable<Detail[]> {
+    return this.http.get<Detail[]>(`${this.API}/details/profile`);
   }
 
-  getDetailsByType(type: string): Observable<Detail[]>{
-    return this.http.get<Detail[]>(`${this.API}/details/${type}`)
+  getDetailsByType(type: string): Observable<Detail[]> {
+    return this.http.get<Detail[]>(`${this.API}/details/${type}`);
   }
 
-  getContactDetails(): Observable<Detail[]>{
-    return this.http.get<Detail[]>(`${this.API}/details/contact`)
+  getContactDetails(): Observable<Detail[]> {
+    return this.http.get<Detail[]>(`${this.API}/details/contact`);
   }
 
-  getSkillByID(id: string): Observable<Skill>{
+  getSkillByID(id: string): Observable<Skill> {
     let encodedID = encodeURIComponent(id);
-    return this.http.get<Skill>(`${this.API}/skills/${encodedID}`)
+    return this.http.get<Skill>(`${this.API}/skills/${encodedID}`);
   }
 
-  getContentBlock(selector: string): Observable<ContentBlock>{
-    return this.http.get<ContentBlock>(`${this.API}/content/${selector}`)
+  getContentBlock(selector: string): Observable<ContentBlock> {
+    return this.http.get<ContentBlock>(`${this.API}/content/${selector}`);
   }
 
-  getContentGroup(selector: string): Observable<ContentBlock[]>{
-    return this.http.get<ContentBlock[]>(`${this.API}/content/group/${selector}`)
+  getContentGroup(selector: string): Observable<ContentBlock[]> {
+    return this.http.get<ContentBlock[]>(
+      `${this.API}/content/group/${selector}`,
+    );
   }
 
-  getDisplayedSkills(): Observable<Skill[]>{
-    return this.http.get<Skill[]>(`${this.API}/skills`)
+  getDisplayedSkills(): Observable<Skill[]> {
+    return this.http.get<Skill[]>(`${this.API}/skills`);
   }
 
-  getAllSkills(): Observable<Skill[]>{
-    return this.http.get<Skill[]>(`${this.API}/skills`)
+  getAllSkills(): Observable<Skill[]> {
+    return this.http.get<Skill[]>(`${this.API}/skills`);
   }
 
-  getSkillsByLevel(): Observable<Skill[]>{
-    return this.http.get<Skill[]>(`${this.API}/skills/byLevel`)
+  getSkillsByLevel(): Observable<Skill[]> {
+    return this.http.get<Skill[]>(`${this.API}/skills/byLevel`);
   }
 
-  getSkillByName(name: string): Observable<Skill>{
-    return this.http.get<Skill>(`${this.API}/skills/byName/${name}`)
+  getSkillByName(name: string): Observable<Skill> {
+    return this.http.get<Skill>(`${this.API}/skills/byName/${name}`);
   }
 
-  getProjects(): Observable<Project[]>{
-    return this.http.get<Project[]>(`${this.API}/projects`)
+  getProjects(): Observable<Project[]> {
+    return this.http.get<Project[]>(`${this.API}/projects`);
   }
 
-  getProjectsBySkill(skillID: string): Observable<ProjectStub[]>{
-    return this.http.get<ProjectStub[]>(`${this.API}/projects/bySkill/${skillID}`)
+  getCVProjects(): Observable<Project[]> {
+    return this.http.get<Project[]>(`${this.API}/projects/forCV`);
   }
 
-  getProjectBySlug(slug: string): Observable<Project>{
-    return this.http.get<Project>(`${this.API}/projects/${slug}`)
+  getProjectsBySkill(skillID: string): Observable<ProjectStub[]> {
+    return this.http.get<ProjectStub[]>(
+      `${this.API}/projects/bySkill/${skillID}`,
+    );
   }
 
-  getEducation(): Observable<Education[]>{
-    return this.http.get<Education[]>(`${this.API}/schools`)
+  getProjectBySlug(slug: string): Observable<Project> {
+    return this.http.get<Project>(`${this.API}/projects/${slug}`);
   }
 
+  getEducation(): Observable<Education[]> {
+    return this.http.get<Education[]>(`${this.API}/schools`);
+  }
 }
