83a2a4ea41090c00a93bc874465d0994f5f992e0
Emrys Corbin <emrys.corbin@gmail.com>
2024-10-08 18:35:39 -0500

    Update styles

--- src/app/constants/project.consts.ts
+++ src/app/constants/project.consts.ts
@@ -1,14 +1,22 @@
 export const ProjectTypes = [
-  {id: "web", label: "Web"},
-  {id: "desktop", label: "Desktop"},
-  {id: "mobile", label: "Mobile"},
-  {id: "game", label: "Game"},
-  {id: "multi", label: "Multi"}
-]
+  { id: 'web', label: 'Web' },
+  { id: 'desktop', label: 'Desktop' },
+  { id: 'mobile', label: 'Mobile' },
+  { id: 'game', label: 'Game' },
+  { id: 'multi', label: 'Multi' },
+];
+
+export const ArticleTypes = [
+  { id: 'announcement', label: 'Announcements' },
+  { id: 'news', label: 'News' },
+  { id: 'tutorial', label: 'Tutorials' },
+  { id: 'blog', label: 'Blogs' },
+  { id: 'review', label: 'Reviews' },
+];
 
 export const ProjectGroups = [
-  {id: "personal", label: "Personal"},
-  {id: "professional", label: "Professional"},
-  {id: "educational", label: "Educational"},
-  {id: "open source", label: "Open Source"},
-]
+  { id: 'personal', label: 'Personal' },
+  { id: 'professional', label: 'Professional' },
+  { id: 'educational', label: 'Educational' },
+  { id: 'open source', label: 'Open Source' },
+];
