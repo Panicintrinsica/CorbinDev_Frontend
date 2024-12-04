d1a9db0ec3e8f5b314447196927262894e0c1a2f
Emrys Corbin <emrys.corbin@gmail.com>
2024-12-04 02:21:19 -0600

    setup button controlled sections on bio.

--- src/app/modules/biography/biography.component.ts
+++ src/app/modules/biography/biography.component.ts
@@ -1,14 +1,20 @@
-import {Component} from '@angular/core';
-import {ServerService} from "../../services/server.service";
-import {MarkdownComponent} from "ngx-markdown";
-import {AsyncPipe, NgClass, TitleCasePipe} from "@angular/common";
-import {MatSlideToggle} from "@angular/material/slide-toggle";
-import {FormsModule} from "@angular/forms";
-import {SkillListComponent} from "../skills/skill-list/skill-list.component";
-import {Detail} from '../../models/detail.model';
-import {Observable} from "rxjs";
-import {ContentBlock} from "../../models/content.model";
-import {getContentBody, getDetail} from "../../utilities";
+import { Component } from '@angular/core';
+import { ServerService } from '../../services/server.service';
+import { MarkdownComponent } from 'ngx-markdown';
+import { AsyncPipe, NgClass } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Detail } from '../../models/detail.model';
+import { Observable } from 'rxjs';
+import { ContentBlock } from '../../models/content.model';
+import { getContentBody, getDetail } from '../../utilities';
+import { MatButton } from '@angular/material/button';
+import { BioBooksComponent } from './sections/bio-books/bio-books.component';
+import { BioDrinksComponent } from './sections/bio-drinks/bio-drinks.component';
+import { BioEngineerComponent } from './sections/bio-engineer/bio-engineer.component';
+import { BioFashionComponent } from './sections/bio-fashion/bio-fashion.component';
+import { BioFoodComponent } from './sections/bio-food/bio-food.component';
+import { BioGamesComponent } from './sections/bio-games/bio-games.component';
+import { BioMusicComponent } from './sections/bio-music/bio-music.component';
 
 @Component({
   selector: 'app-biography',
@@ -16,27 +22,47 @@ import {getContentBody, getDetail} from "../../utilities";
   imports: [
     MarkdownComponent,
     AsyncPipe,
-    NgClass,
-    TitleCasePipe,
-    MatSlideToggle,
     FormsModule,
-    SkillListComponent
+    MatButton,
+    BioBooksComponent,
+    BioDrinksComponent,
+    BioEngineerComponent,
+    BioFashionComponent,
+    BioFoodComponent,
+    BioGamesComponent,
+    BioMusicComponent,
+    NgClass,
   ],
   templateUrl: './biography.component.html',
-  styleUrl: './biography.component.scss'
+  styleUrl: './biography.component.scss',
 })
 export class BiographyComponent {
-
   content$: Observable<ContentBlock[]>;
 
   details: any = {};
   details$: Observable<Detail[]>;
 
+  sections: { id: string; title: string }[] = [
+    { id: 'books', title: 'Books' },
+    { id: 'drinks', title: 'Drinks' },
+    { id: 'engineer', title: 'Engineering' },
+    { id: 'fashion', title: 'Fashion' },
+    { id: 'food', title: 'Food' },
+    { id: 'games', title: 'Games' },
+    { id: 'music', title: 'Music' },
+  ];
+
+  selection: string = '';
+
   constructor(private server: ServerService) {
-    this.content$ = server.getContentGroup("bio")
-    this.details$ = server.getAllDetails()
+    this.content$ = server.getContentGroup('bio');
+    this.details$ = server.getAllDetails();
   }
 
   protected readonly getContentBody = getContentBody;
   protected readonly getDetail = getDetail;
+
+  makeSelection(id: string) {
+    this.selection = id;
+  }
 }
